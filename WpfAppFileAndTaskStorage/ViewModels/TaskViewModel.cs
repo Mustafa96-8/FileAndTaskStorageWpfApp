1b7f816d94b0257e913feb7fa7d46af0cdf9e2a9
Mustafa96-8 <81869299+Mustafa96-8@users.noreply.github.com>
2024-09-28 00:02:01 +0500

    TaskViewModel has been Created & TaskView has been Modified
    Создана модель отображения для Задачи и написано отображение для неё

---
+++ WpfAppFileAndTaskStorage/ViewModels/TaskViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WpfAppFileAndTaskStorage.Models;
+using WpfAppFileAndTaskStorage.MVVM;
+
+namespace WpfAppFileAndTaskStorage.ViewModels
+{
+    public class TaskViewModel : BaseViewModel
+    {
+        #region Поля и свойства
+        public Task Task { get; set; }
+        
+        private IEnumerable<TaskStatus> statuses;
+
+        public IEnumerable<TaskStatus> Statuses
+        {
+            get => statuses;  
+            set
+            {
+                SetProperty(ref statuses, value);
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                SetProperty(ref name, value);
+                this.Task.SetName(value);
+            }
+        }
+
+        private string body;
+
+        public string Body
+        {
+            get => body;
+            set
+            {
+                SetProperty(ref body, value);
+                Task.SetBody(value);
+            }
+        }
+
+        private TaskStatus status;
+        public TaskStatus Status
+        {
+            get => status;
+            set
+            {
+                SetProperty(ref status, value);
+                this.Task.SetStatus(value);
+            }
+        }
+
+        public string TypeName => "Задача"; 
+        #endregion
+
+        public TaskViewModel(Task task)
+        {
+            this.Statuses = Enum.GetValues(typeof(TaskStatus)).Cast<TaskStatus>();
+            this.Task = task;
+            this.Status = task.Status;
+            this.Body = task.Body;
+            this.Name = task.Name;
+        }
+    }
+}
