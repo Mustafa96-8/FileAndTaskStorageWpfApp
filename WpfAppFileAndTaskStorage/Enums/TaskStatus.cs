85adf82067c6459b24eec20a1adccaeff0c5ad33
Mustafa96-8 <81869299+Mustafa96-8@users.noreply.github.com>
2024-09-30 13:45:53 +0500

    Create TaskStatus.cs
    Статусы задачи вынесены в отдельный файл

---
+++ WpfAppFileAndTaskStorage/Enums/TaskStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfAppFileAndTaskStorage.Enums
+{
+    /// <summary>
+    /// Перечисление, определяющее возможные статусы задачи.
+    /// </summary>
+    public enum TaskStatus
+    {
+        /// <summary>
+        /// Задача находится в работе.
+        /// </summary>
+        AtWork,
+
+        /// <summary>
+        /// Задача завершена.
+        /// </summary>
+        Completed
+    }
+}
